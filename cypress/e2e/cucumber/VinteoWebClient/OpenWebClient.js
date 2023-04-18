6063bb4beea56a8d1233f8f154ddbd6015d8f4c5
KonstantinSeiler <ssteammoi@mail.ru>
2023-04-18 10:21:40 +0300

    Vinteo

--- cypress/e2e/cucumber/VinteoWebClient/OpenWebClient.js
+++ cypress/e2e/cucumber/VinteoWebClient/OpenWebClient.js
@@ -1,1 +1,6 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+
+Given('Пользователь перешел на сайт', () => {
+    cy.visit(`https://10.23.8.92/client?conference=999993`);
+});
+
