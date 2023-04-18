a86e2292ea76c37fe060f54a5b77ecd66279f757
KonstantinSeiler <ssteammoi@mail.ru>
2023-04-18 17:57:24 +0300

    lastovich

--- cypress/e2e/cucumber/VinteoWebClient/OpenWebClient.js
+++ cypress/e2e/cucumber/VinteoWebClient/OpenWebClient.js
@@ -4,3 +4,7 @@ Given('Пользователь перешел на сайт', () => {
     cy.visit(`https://10.23.8.92/client?conference=999993`);
 });
 
+Then('Открыта страница', () => {
+    cy.get('#wrap3').should('exist')
+});
+
