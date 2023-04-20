6f960001f40d15ad04fa625656b3b3ba74a45e2e
KonstantinSeiler <ssteammoi@mail.ru>
2023-04-20 16:08:25 +0300

    LoginVinteo

--- cypress/e2e/cucumber/VinteoWebClient/OpenWebClient.js
+++ cypress/e2e/cucumber/VinteoWebClient/OpenWebClient.js
@@ -4,7 +4,26 @@ Given('Пользователь перешел на сайт', () => {
     cy.visit(`https://10.23.8.92/client?conference=999993`);
 });
 
+
+
+When('Пользователь нажал на строку ввода имени', () => {
+    cy.get('#input9').click()
+});
+
+
+
+
+
+
+
+
+
+
 Then('Открыта страница', () => {
     cy.get('.layout__workspace__content').should('exist')
 });
 
+
+Then('Доступно поле ввода', () => {
+    cy.get('#input9').should('exist')
+});
