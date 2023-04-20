481474a99f66a6cd060d81cd11dc857d7f0ca96d
KonstantinSeiler <ssteammoi@mail.ru>
2023-04-20 17:53:38 +0300

    Dom for Cam

--- cypress/e2e/cucumber/VinteoWebClient/camMain.js
+++ cypress/e2e/cucumber/VinteoWebClient/camMain.js
@@ -2,10 +2,10 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 import dom from '../dom'
 
 Given('Пользователь перешел на сайт', () => {
-    cy.visit(`https://10.23.8.92/client?conference=999993`);
+    dom.visitByLinkUrl(`https://10.23.8.92/client?conference=999993`);
 });
 
-When('Пользователь нажал на выключение камеры', () => {
+When('Пользователь выключил камеру', () => {
     dom.getByTestId('callActionsVideocam').click()
 });
 
--- cypress/e2e/cucumber/dom.ts
+++ cypress/e2e/cucumber/dom.ts
@@ -7,9 +7,23 @@ const getByClassName = (className) => {
     return  cy.get(`.${className}`)
 }
 
+const getByNameId = (nameId) => {
+    return cy.get(`#${nameId}`)
+}
+
+const getByHrefLink = (hrefLink) => {
+    return cy.get(`a${hrefLink}`)
+}
+
+const visitByLinkUrl = (linkUrl) => {
+    return cy.visit(`${linkUrl}`)
+}
 const dom = {
     getByTestId, 
     getByClassName,
+    getByNameId,
+    visitByLinkUrl,
+
 }; 
 
 
